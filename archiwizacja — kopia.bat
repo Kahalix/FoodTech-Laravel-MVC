628bffcefbb0981bd8ce7f01cd152524965e2f73
Kahalix <110099525+Kahalix@users.noreply.github.com>
2025-01-05 23:41:43 +0100

    Delete archiwizacj

--- archiwizacja — kopia.bat
+++
@@ -1,7 +0,0 @@
-git init
-git config --local user.name "Kahalix"
-git config --local user.email "kahalix@gmail.com"
-git add --all
-git commit -m "FoodTech-LaravelMVC"
-git archive --format=zip HEAD -o ../foodtech.zip
-pause
